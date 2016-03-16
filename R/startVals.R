335a04ddb1560918f0c7f0c71b96ecc01bbad788
Heath Blackmon <coleoguy@gmail.com>
2016-03-16 13:57:19 -0500

    add help files

--- R/startVals.R
+++ R/startVals.R
@@ -5,8 +5,10 @@
 
 # takes n for the number of values needed and 
 # samples from a uniform distribution between l and h
-startVals <- function(n, l, h){
-  start.vals <- runif(n, min=l, max=h)
+startVals <- function(n, min, max, mean, sd, dist="unif"){
+  if(!dist %in% c("unif", "norm")) stop("Specified distribution not implemented")
+  if(dist=="unif") start.vals <- runif(n, min=min, max=max)
+  if(dist=="norm") start.vals <- rnorm(n, mean=mean, sd=sd)
   return(start.vals)
 }
 
