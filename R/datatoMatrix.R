b39a17b6845bb29592a0ee78b5dcf78d789b0216
Heath Blackmon <coleoguy@gmail.com>
2016-03-17 17:36:59 -0500

    fixes

--- R/datatoMatrix.R
+++ R/datatoMatrix.R
@@ -8,12 +8,12 @@
 # polyploids: if T then an unobserved state for polypoidy
 # is created if F then basically you have the older style 
 # chromevol.
-datatoMatrix <- function(x, excess = .1, hidden = T){
-  matsize <- round(max(as.numeric(x[, 2])) * (1 + excess))
-  if(hidden == T){
+datatoMatrix <- function(x, range, hyper = T){
+  matsize <- range[2]-range[1]+1
+  if(hyper == T){
     dmat <- matrix(0, nrow(x), matsize * 2)
     states <- c(as.character(1:matsize),
-                paste(as.character(1:matsize), "p", sep = ""))
+                paste(as.character(1:matsize), "h", sep = ""))
     colnames(dmat) <- states
     row.names(dmat) <- x[, 1]
     for(i in 1:nrow(x)){
@@ -21,7 +21,7 @@ datatoMatrix <- function(x, excess = .1, hidden = T){
       dmat[i, which(colnames(dmat) == x[i, 2]) + matsize] <- .5
     }
   }
-  if(hidden == F){
+  if(hyper == F){
     dmat <- matrix(0, nrow(x), matsize)
     states <- as.character(1:matsize)
     colnames(dmat) <- states
