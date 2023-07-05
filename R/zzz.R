1a5dbd13e6aa40554890455a6cc9883f1222a3e6
Michelle Jonika <Michelle19@tamu.edu>
2023-07-05 11:07:02 -0500

    Add citation message when loaded

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+.onAttach <- function(libname, pkgname) {
+      packageStartupMessage("\nThank you for using chromePlus!\n")
+      packageStartupMessage("\nTo acknowledge our work, please cite the package: \n")
+      packageStartupMessage(" Blackmon, H., M. Chin, and M. Jonika. (2023). chromePlus: An R package for the analysis of chromosome number evolution.")
+      packageStartupMessage(" R package version 2.0. https://github.com/coleoguy/chromePlus \n")
+    }
+  
