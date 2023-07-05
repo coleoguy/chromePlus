c7f5541c753dd706436fa5355f5094f480c10203
Michelle Jonika <Michelle19@tamu.edu>
2023-07-05 12:08:57 -0500

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,12 @@
 .onAttach <- function(libname, pkgname) {
       packageStartupMessage("\nThank you for using chromePlus!\n")
       packageStartupMessage("\nTo acknowledge our work, please cite the package: \n")
-      packageStartupMessage(" Heath Blackmon, Maximos Chin, and Michelle Jonika. (2023). chromePlus: Analysis of Chromosome Number Evolution and Binary Traits.")
+      packageStartupMessage(" Heath Blackmon, Maximos Chin, and Michelle Jonika. (2023).") 
+      packageStartupMessage(" chromePlus: Analysis of Chromosome Number Evolution and Binary Traits.")
       packageStartupMessage(" coleoguy/chromePlus: chromePlus version 2.0 \n")
       
-      packageStartupMessage(" Blackmon, H., Justison, J., Mayrose, I. and Goldberg, E.E. (2019). Meiotic drive shapes rates of karyotype evolution in mammals.") 
+      packageStartupMessage(" Blackmon, H., Justison, J., Mayrose, I. and Goldberg, E.E. (2019).") 
+      packageStartupMessage(" Meiotic drive shapes rates of karyotype evolution in mammals.") 
       packageStartupMessage(" Evolution, 73(3), pp.511-523. \n")
     }
   
